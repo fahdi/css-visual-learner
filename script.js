2ec5768de02d558d373bd47246a37f4204049ffb
isupercoder <info@fahdmurtaza.com>
2023-11-22 19:36:46 +0500

    Enhance interactive elements and styling
    - Add a new interactive element in index.html for demonstrating font changes.
    - Implement getRandomColor function in script.js for dynamic background colors.
    - Adjust body padding and size of interactive elements in style.css.

--- script.js
+++ script.js
@@ -1,11 +1,33 @@
-document.querySelectorAll('.interactive-element').forEach(element => {
-  element.addEventListener('mouseover', function(){
-    this.style.backgroundColor = 'yellow';
-    document.getElementById('css-rule-display').innerText = 'background-color: yellow;';
+function getRandomColor() {
+  const letters = '0123456789ABCDEF';
+  let color = '#';
+  for (let i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
+}
+
+document.querySelectorAll('.interactive-element').forEach((element, index) => {
+  element.addEventListener('mouseover', function() {
+    let color;
+    if (index === 3) {
+      this.style.color = 'blue';
+      this.style.fontSize = '20px';
+      document.getElementById('css-rule-display').innerText = 'color: blue; font-size: 20px;';
+    } else {
+      color = getRandomColor();
+      this.style.backgroundColor = color;
+      document.getElementById('css-rule-display').innerText = `background-color: ${color};`;
+    }
   });
 
-  element.addEventListener('mouseout', function(){
-    this.style.backgroundColor = '';
+  element.addEventListener('mouseout', function() {
+    if (index === 3) {
+      this.style.color = '';
+      this.style.fontSize = '';
+    } else {
+      this.style.backgroundColor = '';
+    }
     document.getElementById('css-rule-display').innerText = 'Hover over the boxes below to see CSS effects!';
   });
 });
