72f7988b7303612200d96bc02dab261cd87384d2
imran khan <imranloveh01@gmail.com>
2023-11-24 00:14:25 +0500

    Refactor flexbox alignment in HTML and JavaScript

--- scripts/flexbox.js
+++ scripts/flexbox.js
@@ -1,11 +1,17 @@
-document.addEventListener('DOMContentLoaded', () => {
-  document.getElementById('flexDirection').addEventListener('change', function(e){
-    document.querySelector('.flex-container').style.flexDirection = e.target.value;
-
-    document.getElementById('alignItems').addEventListener('change',function(e){
-      document.querySelector('.flex-container').style.alignItems = e.target.value;
-    })
-  });
+document.addEventListener("DOMContentLoaded", () => {
+  document
+    .getElementById("flexDirection")
+    .addEventListener("change", function (e) {
+      document.querySelector(".flex-container").style.flexDirection =
+        e.target.value;
+    });
+  document
+    .getElementById("alignItems")
+    .addEventListener("change", function (e) {
+      document.querySelector(
+        "#alignItemsVisualizer .flex-container"
+      ).style.alignItems = e.target.value;
+    });
 });
 
 // Add similar event listeners for other Flexbox properties
