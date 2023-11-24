420e2ead4d51d2f9c3258f759f6b2e9c4013b054
Fahad Murtaza <info@fahdmurtaza.com>
2023-11-25 03:35:49 +0500

    Enhance Learning Experience with Interactive Flexbox Justify-Content Visualizer (#45)
    * Add an extended grid example.
    
    * Updated extended grid page title
    
    * Add interactive Flexbox justify-content visualizer
    
    - Implement a new visualizer for Flexbox justify-content property
    - Include controls for adjusting container width and triggering animations
    - Enhance sidebar navigation with a direct link to the new visualizer
    
    * Updated page title for Flex - justify-content property
    
    * Resolve Issue: Implement 'align-content' Section in Flexbox Visualizer (#40)
    
    * Refactor flexbox layout and add align-items
    control
    
    * Add styles for alignItems and controls select
    
    * Update flexbox alignment in HTML and JavaScript
    files
    
    * Refactor flexbox styles
    
    * Refactor flexbox alignment in HTML and JavaScript
    
    * Fix formatting in flex-box.html
    
    * Add justify-content functionality to flexbox
    
    * Fix flex-box layout
    
    * Remove unnecessary code in flex-box.html
    
    * Add align-content functionality to flexbox
    
    * implementing grid-layout module (#43)
    
    * Resolved conflicts
    
    ---------
    
    Co-authored-by: Imran khan <70023539+imran-khani@users.noreply.github.com>
    Co-authored-by: Ali Aais <99737152+aais1@users.noreply.github.com>

--- scripts/flexbox.js
+++ scripts/flexbox.js
@@ -26,6 +26,34 @@ document.addEventListener('DOMContentLoaded', () => {
       ).style.alignContent = e.target.value;
     })
 
-});
+  // Get the range input and animate button
+  const rangeInput = document.getElementById('container-width');
+  const animateButton = document.getElementById('animate-width');
 
-// Add similar event listeners for other Flexbox properties
+  // Function to update the width of the flex containers
+  function updateWidth(value){
+    document.querySelectorAll('.flex-container').forEach(container => {
+      container.style.width = value + 'px';
+    });
+  }
+
+  // Event listener for the range input
+  rangeInput.addEventListener('input', (e) => {
+    updateWidth(e.target.value);
+  });
+
+  // Function to animate the flex containers
+  function animateWidth(){
+    // Toggle a class that changes the width over time
+    document.querySelectorAll('.flex-container').forEach(container => {
+      container.classList.add('animate-width');
+      // Remove the class after the animation ends
+      container.addEventListener('animationend', () => {
+        container.classList.remove('animate-width');
+      }, { once: true });
+    });
+  }
+
+  // Event listener for the animate button
+  animateButton.addEventListener('click', animateWidth);
+});
