a16fd8706cec43ec9f8d5469a932edada68bb777
Imran khan <70023539+imran-khani@users.noreply.github.com>
2023-11-24 19:47:15 +0500

    Added 'justify-content' Section to Flexbox Visualizer (#22)
    * Refactor flexbox layout and add align-items
    control
    
    * Add styles for alignItems and controls select
    
    * Update flexbox alignment in HTML and JavaScript
    files
    
    * Refactor flexbox styles
    
    * Refactor flexbox alignment in HTML and JavaScript
    
    * Fix formatting in flex-box.html
    
    * Add justify-content functionality to flexbox
    
    * Fix flex-box layout
    
    * Remove unnecessary code in flex-box.html

--- scripts/flexbox.js
+++ scripts/flexbox.js
@@ -1,7 +1,24 @@
-document.addEventListener('DOMContentLoaded', () => {
-  document.getElementById('flexDirection').addEventListener('change', function(e){
-    document.querySelector('.flex-container').style.flexDirection = e.target.value;
-  });
+document.addEventListener("DOMContentLoaded", () => {
+  document
+    .getElementById("flexDirection")
+    .addEventListener("change", function (e) {
+      document.querySelector(".flex-container").style.flexDirection =
+        e.target.value;
+    });
+  document
+    .getElementById("alignItems")
+    .addEventListener("change", function (e) {
+      document.querySelector(
+        "#alignItemsVisualizer .flex-container"
+      ).style.alignItems = e.target.value;
+    });
+  document
+    .getElementById('justifyContent')
+    .addEventListener('change', (e) => {
+      document.querySelector('#justifyContentVisualizer .flex-container'
+      ).style.justifyContent = e.target.value
+    })
+
 });
 
 // Add similar event listeners for other Flexbox properties
