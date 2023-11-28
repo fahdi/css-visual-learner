8aa9536d1d4a2d71e13ef4ff49c949fe24f2aca4
AliCodingGuru <alihassan2023@protonmail.com>
2023-11-28 01:03:37 -0500

    added copy styling feature,custom elements selector for visualizer in grid section

--- scripts/grid.js
+++ scripts/grid.js
@@ -4,14 +4,20 @@ document.addEventListener('DOMContentLoaded', function(){
   const gridColumnGapInput = document.getElementById('gridColumnGap');
   const gridRowGapInput = document.getElementById('gridRowGap');
   const gridContainer = document.querySelector('.grid-container');
-
+  const codeDisplayer = document.querySelector('#csscode p')
+  const CopyStyleButton = document.querySelector('#csscode button')
+  const NoOfContainers = document.querySelector('.controls input')
   function applyStyles(){
-    gridContainer.style.cssText = `
+    var css = gridContainer.style.cssText = `
         grid-template-columns: ${gridTemplateColumnsInput.value};
         grid-template-rows: ${gridTemplateRowsInput.value};
         column-gap: ${gridColumnGapInput.value};
         row-gap: ${gridRowGapInput.value};
       `;
+    codeDisplayer.textContent = css
+  }
+  function copycss(){
+    navigator.clipboard.writeText(gridContainer.style.cssText)
   }
 
   // Event listeners for input changes
@@ -19,5 +25,15 @@ document.addEventListener('DOMContentLoaded', function(){
   gridTemplateRowsInput.addEventListener('input', applyStyles);
   gridColumnGapInput.addEventListener('input', applyStyles);
   gridRowGapInput.addEventListener('input', applyStyles);
+  NoOfContainers.addEventListener('input',() => {
+    gridContainer.innerHTML = ''
+    for (let i = 1; i < parseInt(NoOfContainers.value)+1; i++) {
+      gridContainer.innerHTML+=`
+      <div class="grid-item">${i}</div>
+      
+      `
+    }
+  })
+  CopyStyleButton.addEventListener('click',copycss)
 });
   
