712adae7831e4a50bc3655ebed54cf0d7e0cdb3e
Ali Hassan <124016531+AliCodingGuru@users.noreply.github.com>
2023-12-04 00:04:39 -0500

    Enhanced Grid.js

--- scripts/grid.js
+++ scripts/grid.js
@@ -1,23 +1,26 @@
-document.addEventListener('DOMContentLoaded', function(){
+document.addEventListener('DOMContentLoaded', function () {
   const gridTemplateColumnsInput = document.getElementById('gridTemplateColumns');
   const gridTemplateRowsInput = document.getElementById('gridTemplateRows');
   const gridColumnGapInput = document.getElementById('gridColumnGap');
   const gridRowGapInput = document.getElementById('gridRowGap');
   const gridContainer = document.querySelector('.grid-container');
-  const codeDisplayer = document.querySelector('#csscode p')
-  const CopyStyleButton = document.querySelector('#csscode button')
-  const NoOfContainers = document.querySelector('.controls input')
-  function applyStyles(){
-    var css = gridContainer.style.cssText = `
+  const codeDisplayer = document.querySelector('#css-code p');
+  const copyStyleButton = document.querySelector('#css-code button');
+  const noOfContainersInput = document.querySelector('.controls input');
+
+  function applyStyles() {
+    let css = gridContainer.style.cssText = `
         grid-template-columns: ${gridTemplateColumnsInput.value};
         grid-template-rows: ${gridTemplateRowsInput.value};
         column-gap: ${gridColumnGapInput.value};
         row-gap: ${gridRowGapInput.value};
       `;
-    codeDisplayer.textContent = css
+    
+    codeDisplayer.textContent = css;
   }
-  function copycss(){
-    navigator.clipboard.writeText(gridContainer.style.cssText)
+
+  function copyCss() {
+    navigator.clipboard.writeText(gridContainer.style.cssText);
   }
 
   // Event listeners for input changes
@@ -25,15 +28,15 @@ document.addEventListener('DOMContentLoaded', function(){
   gridTemplateRowsInput.addEventListener('input', applyStyles);
   gridColumnGapInput.addEventListener('input', applyStyles);
   gridRowGapInput.addEventListener('input', applyStyles);
-  NoOfContainers.addEventListener('input',() => {
-    gridContainer.innerHTML = ''
-    for (let i = 1; i < parseInt(NoOfContainers.value)+1; i++) {
-      gridContainer.innerHTML+=`
+
+  noOfContainersInput.addEventListener('input', function () {
+    gridContainer.innerHTML = '';
+    for (let i = 1; i < parseInt(noOfContainersInput.value) + 1; i++) {
+      gridContainer.innerHTML += `
       <div class="grid-item">${i}</div>
-      
-      `
+      `;
     }
-  })
-  CopyStyleButton.addEventListener('click',copycss)
+  });
+
+  copyStyleButton.addEventListener('click', copyCss);
 });
-  
